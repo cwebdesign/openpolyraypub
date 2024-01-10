af478f464b7179281cb6b5af5b8f703eec349512
Cweb <cwebdesign@users.noreply.github.com>
2024-01-10 11:40:17 +0100

    add gpl3 boilerplate

--- src/getopt.h
+++ src/getopt.h
@@ -3,6 +3,19 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 //getopt.h
 //(C) C. Meli 2024
+/* This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #if !defined(__POLYRAY_GETOPT)
 #define __POLYRAY_GETOPT
 #include <string>
